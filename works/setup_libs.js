3bcdc2e28dd4d5d45a20805d064d8562d86854bf
MrDuck0501 <118536780+MrDuck0501@users.noreply.github.com>
2026-02-10 12:07:57 +0800

    Add files via upload

---
+++ works/setup_libs.js
@@ -0,0 +1,7 @@
+var script1 = document.createElement('script');
+script1.src = 'https://cdn.tailwindcss.com';
+document.head.appendChild(script1);
+
+var script2 = document.createElement('script');
+script2.src = 'https://unpkg.com/lucide@latest';
+document.head.appendChild(script2);
